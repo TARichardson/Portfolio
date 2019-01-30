425bd268a28f997d10818ff7ac185fd4fc629f87
Troy A. Richardson <troy.aaron.richardson@gmail.com>
2019-01-30 13:46:10 -0500

    deleteting origin app folder created on macOS

--- portfolio-client/src/component/About.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default () => {
-  return (
-    <div key="AboutDiv" className="MainContent">
-      <h1>About</h1>
-      <p className="AboutP">Knowledge in over eight programming/scripting languages. Recognized for the ability to work under high-pressure deadlines. Customer-focused individual who is acknowledged for talents in inspiring coworkers to excel and adapt to the demands of a challenging workload. Excellent communication and organizational skills along with strong ethics aimed at projecting a positive company image.</p>
-    </div>  )
-}
